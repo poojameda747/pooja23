9bf92e670de53cf30d9085d646f29c506b0c7014
poojameda747 <157389856+poojameda747@users.noreply.github.com>
2024-02-12 18:01:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentDetails from './studentdetails';
+import './Student.css'; // Import Student.css
+
+function App() {
+  return (
+    <div className="App">
+      <StudentDetails />
+    </div>
+  );
+}
+
+export default App;
